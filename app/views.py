74b12544901a418661c98cfb0d6060d38ff3e89c
masunghoon <masunghoon@gmail.com>
2014-09-15 14:38:39 +0900

    2.4 Templates - Loops in templates

--- app/views.py
+++ app/views.py
@@ -5,4 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'nickname': 'Masunghoon' } # fake user
-    return render_template("index.html",  title = 'Home',  user = user)
+    posts = [ # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Korea!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Myeongryang movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)

a03d3e19681d6cd7d7e61051b26ff030f83cb39d
cnsqja6628 <cnsqja6628@naver.com>
2014-09-17 19:51:36 +0900

    20140917_02

--- app/views.py
+++ app/views.py
@@ -1,10 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
-@app.route('/name')
-def name():
-    return "Hello, kim!"
+    user = { 'nickname' : 'Kim' } # fake user
+    return render_template("index.html", title = 'Home', user = user  )

28aa020da0f9ffdb5857934b62728b5c9d0e082a
sanghokim <1808kim@naver.com>
2014-09-17 19:52:17 +0900

    20140917_02

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'DaeGool', 'name':'kimsangho'} # fake user
+    return render_template("index.html", title = 'Home', user = user)

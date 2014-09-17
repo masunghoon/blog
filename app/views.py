9885aa99a2f4541e91611eebd3422d453e31f158
seungri5310 <seungri5310@naver.com>
2014-09-17 19:50:53 +0900

    20140917_02

--- app/views.py
+++ app/views.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname','kang'}
-    return '''
-<html>
-  <head>
-    <title>Home Page  </title>
-  </head>
-  <body>
-    <h1>Hello, '''+user['nickname']+'''</h1>
-  </body>
-</html>
-'''
+    user = {'nickname':'kang'}
+    return render_template("index.html", title='Home'. user = user)

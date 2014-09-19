c52c7e906ccfd17e6953420e7d9573547ae72ce7
kjh970813 <kjh970813@naver.com>
2014-09-19 19:40:07 +0900

    20140919_01

--- app/views.py
+++ app/views.py
@@ -4,6 +4,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname':'Kimjaehoon' }# fake user
+    user = {'nickname':'Kimjaehoon', 'age':18, 'address':'Seoul' }# fake user
     return render_template("Index.html", title = 'Home', user = user)
 

bcc3ff0773d0504ee564592bb2a67fe930b84d42
jojaehyeong <jojaehyeong3729@daum.net>
2014-09-19 19:34:04 +0900

    20140917_02

--- app/views.py
+++ app/views.py
@@ -4,5 +4,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'jaehyeong'} # fake user
-    return render_template("index.html", title = 'Home', user = user)
+    user = { 'nickname': 'jaehyeong' , 'age':18, 'address':'korea'} # fake user
+    return render_template("index.html",
+                           title = 'jaehyeong',
+                           user = user)

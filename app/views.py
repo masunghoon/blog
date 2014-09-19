9d6a214bcce24adffcb870346c853c9170e07b37
kilyunseo <kiljuniver@naver.com>
2014-09-19 19:31:18 +0900

    20140919_01

--- app/views.py
+++ app/views.py
@@ -4,5 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'kilyunseo'}
+    user = {'nickname': 'kilyunseo', 'age': 17, 'address': 'ansan'}
     return render_template("index.html", title = 'home', user = user)

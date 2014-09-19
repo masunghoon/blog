69b3b08f75578b801c3d277e4c25016873cbb131
heesungan <ankim775@naver.com>
2014-09-19 19:31:08 +0900

    20140919_02

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'heesung'}# fake user
+    user = {'name': 'heesung','age':18, 'address':'korea'}# fake user
     return render_template("index.html", title = 'Home', user = user)

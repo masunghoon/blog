601f5b6c874befb482191319d88f9c3aac51d9c9
sanghokim <1808kim@naver.com>
2014-09-22 18:08:32 +0900

    20140922_01

--- app/views.py
+++ app/views.py
@@ -4,7 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'name':'kimsangho', 'age' : 18, 'address' : 'Anyang'} # fake user
+    user = {'name':'Jin Shang Hao', 'age' : 18, 'address' : 'Anyang'} # fake user
+    posts = [
+        {
+            'author': {'nickname': 'John' },
+            'body': 'Beautiful day in Korea!'
+        },
+        {
+            'author': {'nickname': 'Susan' },
+            'body': 'The Myeonryang movie was so cool!'
+        }
+    ]
     return render_template("index.html",
-                           title = 'Home',
-                           user = user)
+         title = 'Home',
+         user = user,
+         posts = posts)

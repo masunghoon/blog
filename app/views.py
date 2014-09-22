3c0edf7663edf2599e164aca04e098e4cbac6848
sw1857 <sw1857@naver.com>
2014-09-22 18:08:18 +0900

    20140919_02

--- app/views.py
+++ app/views.py
@@ -10,6 +10,16 @@ def name():
 @app.route('/index')
 
 def index():
-    user = {'nickname': 'sunwoolim', 'age' : 18, 'address' : 'yangpyeong'}
-    return render_template("index.html", title = 'home', user = user)
+    user = {'nickname': 'sunwoolim'}
+    posts = [
+        {
+            'author':{'nickname': 'john'},
+             'body': 'beautiful day in Korea!'
+        },
+        {
+            'author':{'nickname':'Susan'},
+            'body': 'The Myeongryang movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title = 'home', user = user, posts = posts)
 

429c7ab3e2f06fc4e7dac8eae1dc85de28bba185
kimmoonku <xcvansrb@naver.com>
2014-09-22 18:08:46 +0900

    20140922-01

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
-from flask import  render_template
-from app import  app
+from flask import render_template
+from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname':'Jieun','name':'Kimmoonku'}# fake user
-    return render_template("index.html",
-                           title= 'Home'
-                           user = user)
+    user = {'nickname':'Song'}
+    posts = [
+        {
+            'author':{'nickname':'John'},
+            'body': 'Beautiful day in Korea!'
+        },
+        {
+            'author':{'nickname':'Jieun'},
+            'body':'The Myeongryang movie was so cool!'
+        }
+    ]
+    return  render_template("Index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)

0d4c169e8c4a245566b99f45013cd0ed4ff920bf
seungri5310 <seungri5310@naver.com>
2014-09-22 18:08:26 +0900

    20140922-01

--- app/views.py
+++ app/views.py
@@ -5,4 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname':'kang'}
-    return render_template("index.html", title='Home'. user = user)
+    posts = [
+        {
+            'author': {'nickname': 'john' },
+            'body': 'Beautiful day in korea!'
+        },
+        {
+            'author': { 'nickname':'Susan' },
+            'body':'The Myeongryang movie was so cool!'
+        }
+    ]
+
+    return render_template("index.html",
+                           title='Home',
+                           user = user,
+                           posts = posts)

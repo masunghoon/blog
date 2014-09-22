0955e8c16116d68086a8c8a879b1b1ce8a48311c
yettman12 <yettman12@naver.com>
2014-09-22 18:09:41 +0900

    20140922_01

--- app/views.py
+++ app/views.py
@@ -4,7 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'name':'kimtaesun!','age':18, 'address':'seoul'} #fake user
+    user = {'nickname':'Teasunkim'}
+    posts = [
+        {
+                'author': {'nickname': 'sun'},
+                'body': 'Beautiful day in korea!'
+        },
+        {
+              'author': {'nickname':'seoul'},
+              'body': 'The nyeongrtang movie was so coll!'
+        }
+        ]
     return render_template("index.html",
                           title = 'home',
-                          user = user)
+                          user = user,
+                          posts = posts)

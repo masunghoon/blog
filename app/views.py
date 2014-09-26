72270233da2c40a068e9014bc286c90ebd5eaf65
kilyunseo <kiljuniver@naver.com>
2014-09-26 19:48:49 +0900

    20140919_01

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from flask import render_template
 from app import app
+from forms import LoginForm
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign in', form = form)
 
 @app.route('/')
 @app.route('/index')

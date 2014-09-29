59f506f9f276125a921007a51373878893bde8f2
heesungan <ankim775@naver.com>
2014-09-29 18:05:51 +0900

    20140929_01

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('ID="'+
+               form.openid.data+
+              '", remember_me= ' +
+              str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form = form)
 
 @app.route('/')
 @app.route('/index')
@@ -10,10 +21,10 @@ def index():
         {
            'author':{ 'nickname': 'John'},
            'body': 'Beautiful day in Korea!'
-        },
+       },
         {
            'author': { 'nickname': 'Susan'},
            'body': 'The Myeongryang movie was so cool!'
-        }
+       }
     ]
     return render_template("index.html", title = 'Home', user = user,posts = posts)

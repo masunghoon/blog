c0f09e8a16595aa5988854056fda89d000a68154
tlsehdals222 <tlsehdals222@naver.com>
2014-09-29 18:05:30 +0900

    20140924_01

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('ID="' + form.openid.data + '", remember_me= ' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title = 'Sign In', form = form)
 
 @app.route('/')
 @app.route('/index')

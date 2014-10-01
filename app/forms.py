3862fc95997cd0c0c46cb40fde83ade2aff11bb3
wnsrjf0707 <wnsrjf012@nate.com>
2014-10-01 19:31:15 +0900

    20141001_01

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(Form):
+    id = StringField('id', validators=[DataRequired()])
+    password = PasswordField('password')
+    #openid = TextField('openid', validators= [Required()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
+from forms import LoginForm
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    user = {'id':'kimjungyeul', 'password':'123456'}
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.id.data == user['id'] and form.password.data == user['password']:
+         flash('logged in as ' + form.id.data)
+        return redirect('/index')
+    else:
+        flash('Login Failed!!!!!')
+    return render_template('login.html', title = 'Sign In', form = form)
+
+
 
 @app.route('/')
 @app.route('/index')

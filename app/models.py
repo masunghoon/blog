89163368731037d743728b849bca08698ee87403
masunghoon <masunghoon@gmail.com>
2014-10-20 14:22:52 +0900

    5.2 User Logins - Revisiting our User model

--- app/models.py
+++ app/models.py
@@ -7,6 +7,21 @@ class User(db.Model):
     password = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 

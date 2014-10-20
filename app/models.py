06414c584f07832ccf079924b37c973d5b3ecc74
kilyunseo <kiljuniver@naver.com>
2014-10-20 19:01:07 +0900

    20141020_01

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class user(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))

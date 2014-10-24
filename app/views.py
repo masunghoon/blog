02a0e49183081154de126be62a57dae1238f95aa
cnsqja6628 <cnsqja6628@naver.com>
2014-10-24 19:24:22 +0900

    20141024

--- app/views.py
+++ app/views.py
@@ -1,14 +1,9 @@
-from flask import render_template, flash, redirect
-from app import app
-from forms import LoginForm
+from flask import render_template, flash, redirect, g
 
-@app.route('/login', methods = ['GET','POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('OpenID="'+ form.openid.data +'",remember_me= '+ str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title = 'Sign In', form = form)
+from app import app, lm, db
+from forms import LoginForm, RegisterForm
+from models import User
+
 
 
 @app.route('/')
@@ -25,5 +20,41 @@ def index():
         'body': 'The Myeongryang movie was so cool!'
     }
     ]
-
     return render_template("index.html", title = 'Home', user = user, posts = posts)
+
+
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    user = {'id':'cnsqja6629', 'password':'75695455'}
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if form.email.data == user.email and form.password.data == user.password:
+            flash('logged in as ' + form.email.data)
+            return redirect('/index')
+        else:
+            flash('Login Failes!!!')
+
+    return render_template('login.html', title = 'Sign In', form = form)
+
+
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    form = RegisterForm()
+    user = User()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            flash('E-mail Already exist')
+        elif form.password.data != form.valid_password.data:
+            flash('Password validation failed')
+        else:
+            user = User()
+            user.email = form.email.data
+            user.password = form.password.data
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')
+
+    return render_template('register.html', title = 'Register', form = form)

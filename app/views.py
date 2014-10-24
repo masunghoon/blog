c55288ba62a50807b97bcad90d35596817530536
gnlqkfdb1004 <gnlqkfdb1004@naver.com>
2014-10-24 19:24:19 +0900

    20141024_01

--- app/views.py
+++ app/views.py
@@ -7,28 +7,30 @@ def login():
     user = {'id':'kimminwoo','password':'3265aa'}
     form = LoginForm()
     if form.validate_on_submit():
-       if form.id.data == user['id'] and form.password.data == user['password']:
+        user = User.query.filter_by(email=form.email.data).first()
+       if form.email.data == user.email and form.password.data == user.password:
            flash('logged in as' + form.id.data)
            return redirect('/index')
        else:
            flash('Login Failed!!')
     return render_template('login.html' , title = 'Sign In', form = form)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = { 'nickname':'minwoo!'}
-    posts = [
-        {
-            'author':{'nickname' : 'John'},
-            'body': 'Beautiful day in Korea!'
-        },
-        {
-            'author':{'nickname' : 'Susan'},
-            'body': 'The Myeongrayang movie was so cool!'
-         }
-    ]
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    form = RegisterForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+    if user is not None:
+        flash('E mail Alredy exists')
+    elif form.password.data != form.valid_password.data:
+        flash('Password validation failed')
+    else:
+        user = User()
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
 
-    return render_template("index.html", title = 'Home', user = user, posts = posts)
-
+    return render_template('register.html', title = 'Register', form = form)

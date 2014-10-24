fc8e66ea3b7ed27b76533157dfb95de9f635a769
wnsrjf0707 <wnsrjf012@nate.com>
2014-10-24 19:29:36 +0900

    20141024_01

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,12 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
-    id = StringField('id', validators=[DataRequired()])
+    email = StringField('email')
     password = PasswordField('password')
-    #openid = TextField('openid', validators= [Required()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class RegisterForm(Form):
+    email = StringField('email')
+    password = PasswordField('password')
+    valid_password =  PasswordField('valid_password')
--- app/views.py
+++ app/views.py
@@ -1,20 +1,43 @@
-from flask import render_template, flash, redirect
-from app import app
-from forms import LoginForm
-
-from forms import LoginForm
+from flask import render_template, flash, redirect, g
+from flask.ext.login import login_user, logout_user
+from app import app,lm, db
+from forms import LoginForm, RegisterForm
+from models import User
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    user = {'id':'kimjungyeul', 'password':'123456'}
     form = LoginForm()
     if form.validate_on_submit():
-        if form.id.data == user['id'] and form.password.data == user['password']:
-         flash('logged in as ' + form.id.data)
-        return redirect('/index')
-    else:
-        flash('Login Failed!!!!!')
-    return render_template('login.html', title = 'Sign In', form = form)
+        user = User.query.filter_by(email=form.email.data).first()
+        if form.email.data == user.email and form.password.data == user.password:
+            g.user = user
+            flash(form.email.data+ 'logged in')
+            return  redirect('/index')
+        else:
+            flash('login failed')
+    return render_template('login.html', title = ' Sign ln', form = form)
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    user = User()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            flash('E-mail Already exist')
+        elif form.password.data != form.valid_password.data:
+            flash('Password validation failed')
+        else:
+            user = User()
+            user.email = form.email.data
+            user.password = form.password.data
+            db.session.add(user)
+            db.session.commit()
+            return  redirect('/login')
+
+    return render_template('register.html', title = 'Register', form = form)
+
 
 
 

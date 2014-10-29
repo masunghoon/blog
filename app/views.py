e0f6af56113d41812ef8731b1d75ed3d27e994ca
masunghoon <masunghoon@gmail.com>
2014-10-29 11:37:41 +0900

    6.5 Profile Pages and Avatars - Editing the profile information

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ import re
 from flask import render_template, flash, redirect, g, url_for, session
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, lm, db
-from forms import LoginForm, RegisterForm
+from forms import LoginForm, RegisterForm, EditForm
 from models import User
 from datetime import datetime
 
@@ -113,6 +113,23 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
 
 
 @lm.user_loader

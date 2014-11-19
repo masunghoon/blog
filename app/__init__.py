0750e25778b8d89ec60e7c5af372bd5387cc684f
seungri5310 <seungri5310@naver.com>
2014-11-19 18:36:24 +0900

    20141119

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,9 @@ from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
-
 db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
 
-from app import views
+from app import views, models
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField,PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import TextField,StringField,PasswordField, BooleanField,TextAreaField
+from wtforms.validators import DataRequired,Required, Length
+from app.models import User
 
 class LoginForm(Form):
     email = StringField('email',validators=[DataRequired()])
@@ -12,4 +13,29 @@ class RegisterForm(Form):
     email = StringField('email')
     password = PasswordField('password')
     valid_password = PasswordField('valid_password')
+
+
+class EditForm(Form):
+    nickname = StringField('nickname')
+    about_me = TextAreaField('about_me', validators = [Length(min=0,max=140)])
+
+    def __init__(self, original_nickname, *args, **kwaegs):
+        Form.__init__(self, *args, **kwaegs)
+        self.original_nickname = original_nickname
+
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user != None:
+            self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+            return False
+        return True
+
+
+class PostForm(Form):
+    post = StringField('post', validators=[DataRequired()])
 

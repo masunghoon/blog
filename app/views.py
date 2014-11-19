69e74fd53e29ab8ddd6aee0649ea978e6b6659c6
선우 <sw1857@naver.com>
2014-11-19 18:37:03 +0900

    20141119_00

--- app/views.py
+++ app/views.py
@@ -1,54 +1,180 @@
-from flask import  render_template, flash, redirect
-from app import app, db
-from forms import LoginForm, RegisterForm
-from models import  User
+import re
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'sunwoolim'}
-    posts = [
-        {
-            'author':{'nickname': 'john'},
-             'body': 'beautiful day in Korea!'
-        },
-        {
-            'author':{'nickname':'Susan'},
-            'body': 'The Myeongryang movie was so cool!'
-        }
-    ]
-    return render_template("index.html", title = 'home', user = user, posts = posts)
+from flask import  render_template, flash, redirect, g,url_for, session
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, lm, db
+from forms import LoginForm, RegisterForm, EditForm, PostForm
+from models import  User, Post
+from datetime import datetime
+from config import POSTS_PER_PAGE
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index/<int:page>', methods=['GET', 'POST'])
+@login_required
+def index(page=1):
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now livel')
+        return redirect(url_for('index'))
+    posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
+    return render_template("index.html", title = 'home', form = form, posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    user = {'email' : 'sunwoolim', 'password' : '123456'}
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    # user = {'email' : 'sunwoolim', 'password' : '123456'}
     form = LoginForm()
     if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
         user = User.query.filter_by(email = form.email.data).first()
-        if form.email.data == user.email and form.password.data == user.password:
-            flash('logged in as ' + form.email.data)
+
+        if user is None:
+            flash('Unregistered Email')
+        elif form.email.data == user.email and form.password.data == user.password:
+            remember_me = False
+            if 'remember_me' in session:
+                remember_me = session['remember_me']
+                session.pop('remember_me', None)
+            login_user(user,remember = remember_me)
             return redirect('/index')
+            flash(user.email + 'logged in')
         else:
-            flash('Login Failed!')
+            flash('login failed')
     return render_template('login.html', title = 'Sign In', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegisterForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
-        if user is not None:
-            flash('E-mail Already exists')
+        if re.match(r"^[a-zA-Z0-9._]+\@[a-zA-Z0-9._]+\.[a-zA-Z]{2,}$", form.email.data) is None:
+            flash('Invalid Email address')
+        elif user is not None:
+            flash('Already joined Email')
         elif form.password.data != form.valid_password.data:
             flash('Password validation failed')
         else:
             user = User()
             user.email = form.email.data
             user.password = form.password.data
+            nick = form.email.data.split('@')[0]
+            if user.query.filter_by(nickname=nick).first() == None:
+                user.nickname = nick
+            else:
+                version =1
+                while True:
+                    new_nickname = nick + str(version)
+                    if User.query.filter_by(nickname = new_nickname). first() == None:
+                        break
+                    version += 1
+                user.nickname = new_nickname
+
             db.session.add(user)
             db.session.commit()
+
+            db.session.add(user.follow(user))
+            db.session.commit()
+
+            flash(user.email + ' registered successfully')
             return redirect('/login')
 
     return render_template('register.html', title = 'Regiser', form = form)
 
+@app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
+@login_required
+def user(nickname, page=1):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
+    return render_template('user.html', user=user, posts=posts)
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm(g.user.nickname)
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t follow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow '+ nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname=nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have sotpped following ' + nickname + '.')
+    return redirect(url_for('user', nickname=nickname))
 
--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+##### DATABASE ####################################
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_RECORD_QUERIES = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app_db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# pagination
+POSTS_PER_PAGE = 3
